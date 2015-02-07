ef6b315270ac4335406279332644148fe13b112f
Anuja <anujasarwate@gmail.com>
2015-02-07 18:00:48 -0500

    Song class added

---
+++ src/songLibraryPackage/Song.java
@@ -0,0 +1,24 @@
+package songLibraryPackage;
+
+public class Song {
+
+	String Name;
+	String Artist;
+	String Album;
+	String Year;
+	int index;
+	Song next;
+	
+	
+	// Constructor take only Name and Artist
+	
+	
+	//
+	
+	
+	
+	
+	
+	
+	
+}
