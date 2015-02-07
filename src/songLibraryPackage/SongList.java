02f8b87a1ca9b926697886e2a32b15eb55ec20dc
Anuja <anujasarwate@gmail.com>
2015-02-07 18:04:12 -0500

    added SongList class

---
+++ src/songLibraryPackage/SongList.java
@@ -0,0 +1,38 @@
+package songLibraryPackage;
+
+public class SongList {
+
+	Song front;
+	Song selected;
+	int numSongs;
+	
+	
+	//boolean searchList(Song, Artist){
+	
+	
+	//}
+	
+	
+	
+	// void addSong(Song){
+		//if(searchList(Sing) == true){
+			//return
+	
+		//
+		//else{
+	
+		//}
+	
+	
+	//}
+	
+	
+	
+	//void deleteSong(){
+	
+	
+	//}
+	
+	
+	
+}
